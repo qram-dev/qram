f937c554416a18605dcdc5b71f946bd02182012b
Artalus <artalus-mail@yandex.ru>
2023-05-10 03:00:53 +0400

    update git usages in code

--- src/qram/flow.py
+++ src/qram/flow.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 from typing import Iterable
 
-import qram.git as git
+from qram.git import Git
 from qram import (
     CommitAndBranches,
     collect_staging,
@@ -15,7 +15,7 @@ from qram.web.provider import ProviderRepoApi
 
 logger = getLogger(__name__)
 
-def _merge(pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
+def _merge(git: Git, pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
     logger.info(f'merge started for #{pr_num}')
     pr = gh.get_pr(pr_num)
     branches_global = BranchFormatter(config)
@@ -30,14 +30,14 @@ def _merge(pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
         raise RuntimeError(f'Cannot merge PR-{pr_num}: it is not marked as good')
     if git.branch_exists(branches_pr.bad):
         raise RuntimeError(f'Cannot merge PR-{pr_num}: it is marked as bad')
-    obstacles = list(collect_staging(f'{branches_pr.merge}~1', branches_global.target))
+    obstacles = list(collect_staging(git, f'{branches_pr.merge}~1', branches_global.target))
     if obstacles:
         raise RuntimeError(f'Cannot merge PR-{pr_num}: other PRs present in queue:\n{obstacles}')
 
     # switch in case we are currently on target branch
     logger.info('moving target to HEAD')
     with git.switched_branch(branches_pr.merge):
-        git.check_call(['branch', branches_global.target, '-f', 'HEAD'])
+        git.new_branch(branches_global.target, force=True)
 
     # First push pr branch, then push target, do it in 2 separate pushes. Otherwise github loses
     # its head and displays sillyness in PR commit list
@@ -45,13 +45,14 @@ def _merge(pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
     git.push(pr.branch_head, True)
     logger.info('pushing target')
     git.push(branches_global.target)
-    git.check_call(['branch', '-D',
-        branches_pr.merge, branches_pr.source, branches_pr.rebase, branches_pr.good, pr.branch_head
-    ])
+    git.delete_branch(
+        branches_pr.merge, branches_pr.source, branches_pr.rebase, branches_pr.good, pr.branch_head,
+        force=True
+    )
     logger.info(f'merge completed for #{pr_num}')
 
 
-def prepare(pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
+def prepare(git: Git, pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
     logger.info(f'stage started for #{pr_num}')
     pr = gh.get_pr(pr_num)
     branches_global = BranchFormatter(config)
@@ -60,52 +61,48 @@ def prepare(pr_num: int, gh: ProviderRepoApi, config: Config) -> None:
     # mark original branch location as source, to use it for rebases later
     logger.info('remember source')
     if not git.branch_exists(branches_pr.source):
-        git.check_call(['branch', branches_pr.source, pr.branch_head])
+        git.new_branch(branches_pr.source, pr.branch_head)
 
     # create merge queue branch if it does not exist yet
     logger.info('ensure queue exists')
     if not git.branch_exists(branches_global.queue):
-        git.check_call(['branch', branches_global.queue, branches_global.target])
+        git.new_branch(branches_global.queue, branches_global.target)
 
     # drop whatever state current branch is in right now, rebase it from original
     logger.info('move head to source')
-    git.check_call(['branch', pr.branch_head, branches_pr.source, '-f'])
+    git.new_branch(pr.branch_head, branches_pr.source, force=True)
 
     logger.info('rebase onto queue')
     with git.switched_branch(pr.branch_head):
         # mark current queue head as target for rebase
-        git.check_call(['branch', branches_pr.rebase, branches_global.queue, '-f'])
-        git.check_call(['rebase', branches_pr.rebase])
+        git.new_branch(branches_pr.rebase, branches_global.queue, force=True)
+        git.rebase(branches_pr.rebase)
 
     logger.info('create merge-commit')
     with git.switched_branch(branches_global.queue):
         message = format_merge_message(pr, config)
         author = format_author(pr)
 
-        # `git merge` cannot format author, so use --no-commit + `git commit`
-        git.check_call(['merge', pr.branch_head, '--no-ff', '--no-commit'])
-        git.check_call([
-            '-c', f'user.name={config.merge_template.author.name}',
-            '-c', f'user.email={config.merge_template.author.email}',
-            'commit',
-            '--author', author,
-            '--cleanup=whitespace', '-m', message,
-        ])
-        git.check_call(['branch', '-f', branches_pr.merge, 'HEAD'])
+        git.merge(pr.branch_head, message, author,
+                  config.merge_template.author.name, config.merge_template.author.email)
+        git.new_branch(branches_pr.merge, force=True)
 
     logger.info('push new queue')
     git.push(branches_global.queue)
 
     # once we enqueued branch, it is no longer bad nor good until we receive new status from ci
     logger.info('remove ci markers')
+    to_delete: list[str] = []
     if git.branch_exists(branches_pr.bad):
-        git.check_call(['branch', '-D', branches_pr.bad])
+        to_delete.append(branches_pr.bad)
     if git.branch_exists(branches_pr.good):
-        git.check_call(['branch', '-D', branches_pr.good])
+        to_delete.append(branches_pr.good)
+    if to_delete:
+        git.delete_branch(*to_delete, force=True)
     logger.info(f'stage completed for #{pr_num}')
 
 
-def mark_merge(pr_num: int, config: Config, ci_ok: bool) -> None:
+def mark_merge(git: Git, pr_num: int, config: Config, ci_ok: bool) -> None:
     # FIXME: marking from CI/github will be done upon hash!! NOT upon PR number!
     logger.info(f'mark started for #{pr_num}')
     branches = BranchFormatter(config).pr(pr_num)
@@ -115,16 +112,19 @@ def mark_merge(pr_num: int, config: Config, ci_ok: bool) -> None:
     else:
         add = branches.bad
         remove = branches.good
-    git.check_call(['branch', '-f', add, branches.merge])
+    # git.check_call(['branch', '-f', add, branches.merge])
+    git.new_branch(add, branches.merge, force=True)
     if git.branch_exists(remove):
-        git.check_call(['branch', '-D', remove])
+        git.delete_branch(remove, force=True)
     logger.info(f'mark completed for #{pr_num}')
 
 
-def shake_stage(gh: ProviderRepoApi, config: Config) -> None:
+def shake_stage(git: Git, gh: ProviderRepoApi, config: Config) -> None:
     logger.info('shake started')
     branches_global = BranchFormatter(config)
-    stage = list(reversed(list(collect_staging(branches_global.queue, branches_global.target))))
+    stage = list(reversed(list(
+        collect_staging(git, branches_global.queue, branches_global.target)
+    )))
     logger.info('stage collected:' + ''.join(f'\n - {x}' for x in stage))
     for idx, (hash, branches) in enumerate(stage):
         pr = extract_pr_from_branch_list(branches, config)
@@ -138,13 +138,13 @@ def shake_stage(gh: ProviderRepoApi, config: Config) -> None:
 
         if branches_pr.good in branches:
             logger.info('pr is good, merge it')
-            _merge(pr, gh, config)
+            _merge(git, pr, gh, config)
         elif branches_pr.bad in branches:
             logger.info('pr is bad, rebase remaining queue')
             # only way to get here should be if previous pr on stage was good. Ergo - it was merged,
             # target now points to its merge, and we should rebase onto it
             remaining = stage[idx+1:]
-            _rebase_queue_onto(branches_global.target, remaining, gh, config)
+            _rebase_queue_onto(git, branches_global.target, remaining, gh, config)
             abort = True
         else:
             abort = True
@@ -154,11 +154,11 @@ def shake_stage(gh: ProviderRepoApi, config: Config) -> None:
     logger.info('shake completed, nothing left')
 
 
-def _rebase_queue_onto(target: str, remaining: Iterable[CommitAndBranches],
+def _rebase_queue_onto(git: Git, target: str, remaining: Iterable[CommitAndBranches],
                        gh: ProviderRepoApi, config: Config) -> None:
     logger.info('queue rebase started')
     branches_global = BranchFormatter(config)
-    git.check_call(['branch', '-f', branches_global.queue, target])
+    git.new_branch(branches_global.queue, target, force=True)
     for hash, branches in remaining:
         pr = extract_pr_from_branch_list(branches, config)
         logger.info(f'rebasing {hash} - #{pr}')
@@ -172,5 +172,5 @@ def _rebase_queue_onto(target: str, remaining: Iterable[CommitAndBranches],
             continue
         else:
             logger.info('pr is not bad, re-enqueue it')
-            prepare(pr, gh, config)
+            prepare(git, pr, gh, config)
     logger.info('queue rebase completed')
