020221e9334adb651ef6c649535d480befebb4ee
Artalus <artalus-mail@yandex.ru>
2023-05-09 22:20:36 +0400

    add a system test reconfiguring the webhook in app

--- test/system/test_github.py
+++ test/system/test_github.py
@@ -1,15 +1,16 @@
+from datetime import datetime
 import logging
 import os
 from copy import deepcopy
 from pathlib import Path
-from typing import Generator
+from typing import Callable, Generator
 
 import pytest
 
 import qram.config
 from qram.config import Config
 from qram.web.provider.github import Github, github_api
-from test.system import ServerThread
+from test.system import ServerThread, wait_for
 from .. import chdir
 
 
@@ -31,10 +32,13 @@ def config() -> Config:
     c = deepcopy(qram.config._defaults)
     c.app.hmac = os.environ['QRAM_APP_HMAC']
     c.app.provider = 'github'
+    pem = os.environ.get('QRAM_APP_GITHUB_PEM')
+    if pem is None:
+        pem = Path(os.environ['QRAM_APP_GITHUB_PEM_FILE']).read_text()
     c.app.github = qram.config._CfgGithub(
         app_id = os.environ['QRAM_APP_GITHUB_APP_ID'],
         installation_id = os.environ['QRAM_APP_GITHUB_INSTALLATION_ID'],
-        pem = Path(os.environ['QRAM_APP_GITHUB_PEM_FILE']).read_text().strip(),
+        pem = pem.strip(),
     )
     return c
 
@@ -43,6 +47,20 @@ def api(config: Config) -> Github:
     api = github_api(config)
 
     return api
+
+@pytest.fixture(scope='module')
+def webhook_reconfigured(api: Github, config: Config) -> None:
+    url = os.environ['QRAM_WEBHOOK_URL']
+    logger.info(f'⬜  reconfiguring Github App webhook url to -> {url} ...')
+    payload = dict(
+        url=url,
+        content_type='json',
+        secret=config.app.hmac,
+    )
+    r = api._request('PATCH', 'app/hook/config', json=payload, use_jwt=True)
+    if not r.ok:
+        raise RuntimeError(r.content.decode())
+    logger.info('🟩  webhook reconfigured')
 
 
 @pytest.mark.sysA
@@ -68,3 +86,57 @@ def test_app_start_stop(config: Config) -> None:
     with server_thread:
         pass
         # wait_for((Path(ORG) / REPO / 'README.md').is_file, 'repo was not checked out')
+
+
+@pytest.mark.sysB
+def test_comment_reaction(config: Config, api: Github, caplog: pytest.LogCaptureFixture,
+                          webhook_reconfigured: None) -> None:
+    caplog.set_level(logging.INFO, logger='qram.web')
+    caplog.set_level(logging.INFO, logger='qram.web.server')
+    server_thread = ServerThread(True, config)
+    with server_thread:
+        message = f'beep-boop, i\'m a bot, time is {datetime.now()}'
+        logger.info(f'⬜  posting a new comment to PR #1 in {ORG}/{REPO}...')
+        payload = dict(
+            body=message
+        )
+        r = api.post(f'/repos/{ORG}/{REPO}/issues/1/comments', json=payload)
+        if not r.ok:
+            raise RuntimeError(r.content.decode())
+        new_comment_id = r.json()['id']
+
+        logger.info('🟩  comment posted')
+        logger.info('⬜  check if webhook got it...')
+        try:
+            wait_for(
+                log_contains(caplog, 'this is PR comment'),
+                'webhook never got "comment" event'
+            )
+            logger.info('🟩  got it')
+            logger.info('⬜  check if worker processed it...')
+            wait_for(
+                log_contains(caplog,
+                    'ma, look, event! PrCommentEvent'),
+                'PrCommentEvent never got processed'
+            )
+            logger.info('🟩  processed')
+        finally:
+            logger.info(f'⬜  deleting freshly posted comment {new_comment_id}...')
+            payload = dict(
+                body=message
+            )
+            r = api._request('DELETE', f'/repos/{ORG}/{REPO}/issues/comments/{new_comment_id}')
+            if not r.ok:
+                raise RuntimeError(r.content.decode())
+            logger.info('🟩  comment deleted')
+
+
+def log_contains(capture: pytest.LogCaptureFixture, msg: str) -> Callable[[], bool]:
+    def contains() -> bool:
+        for record in capture.records:
+            if msg in record.message:
+                return True
+        return False
+    return contains
+
+logging.basicConfig(level=logging.INFO)
