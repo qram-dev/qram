4b8c7e7c1c0a70a9881499965598e83256c8d0ac
Artalus <artalus-mail@yandex.ru>
2023-05-10 03:05:40 +0400

    update git usages in tests

--- test/component/test_git.py
+++ test/component/test_git.py
@@ -1,22 +1,21 @@
 from pathlib import Path
 
-import qram.git as git
+from qram.git import Git
 
-from .. import chdir
 
 class TestSwitchingBranches:
     def test_basic(self, repo_tar: Path) -> None:
-        with chdir(repo_tar):
-            assert git.current_branch() == 'main'
-            assert git.branch_exists('do-1')
-            with git.switched_branch('do-1'):
-                assert git.current_branch() == 'do-1'
-            assert git.current_branch() == 'main'
+        git = Git(repo_tar)
+        assert git.current_branch() == 'main'
+        assert git.branch_exists('do-1')
+        with git.switched_branch('do-1'):
+            assert git.current_branch() == 'do-1'
+        assert git.current_branch() == 'main'
 
     def test_anew(self, repo_tar: Path) -> None:
-        with chdir(repo_tar):
-            assert git.current_branch() == 'main'
-            assert not git.branch_exists('foo')
-            with git.switched_branch('foo', anew=True):
-                pass
-            assert git.branch_exists('foo')
+        git = Git(repo_tar)
+        assert git.current_branch() == 'main'
+        assert not git.branch_exists('foo')
+        with git.switched_branch('foo', anew=True):
+            pass
+        assert git.branch_exists('foo')
